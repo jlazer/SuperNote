60616e60f266efa42077a419bc9d934037adbc9a
student1 <student1@jh-mbair-158-06.d214.org>
2016-01-08 10:18:07 -0600

    Testing with TableViews.

--- SuperNote/ViewController.swift
+++ SuperNote/ViewController.swift
@@ -9,18 +9,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    var myArray = ["Tom", "Jim","Bob","Justin", "Sally"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath)
+        cell.textLabel?.text = myArray[indexPath.row]
+        return cell
+        
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myArray.count;
+    }
+    
 }
 
