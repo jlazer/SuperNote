f07fcdf7e9055ce02cdf970512002287ebf71988
student1 <student1@jh-mbair-158-06.d214.org>
2016-01-13 10:17:31 -0600

    adding in a title and notes slot for the alert.

--- SuperNote/ViewController.swift
+++ SuperNote/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var tableViewOutlet: UITableView!
     
     var myArray = ["Tom", "Jim","Bob","Justin", "Sally"]
     
@@ -35,15 +36,23 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         var alert = UIAlertController(title: "Some Title", message: "Enter a text", preferredStyle: .Alert)
         
         //2. Add the text field. You can configure it however you need.
-        alert.addTextFieldWithConfigurationHandler({ (textField) -> Void in
-            textField.text = "Some default text."
+        alert.addTextFieldWithConfigurationHandler({ (titleTextField) -> Void in
+            titleTextField.text = "Title."
+            
+        })
+        
+        alert.addTextFieldWithConfigurationHandler({ (notesTextField) -> Void in
+            notesTextField.text = "Notes."
         })
         
         //3. Grab the value from the text field, and print it when the user clicks OK.
         alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
-            let textField = alert.textFields![0] as UITextField
-            print("Text field: \(textField.text)")
-            self.myArray.append(textField.text!)
+            let titleTextField = alert.textFields![0] as UITextField
+            let notesTextField = alert.textFields![0] as UITextField
+            print("Text field: \(titleTextField.text)")
+            print(
+            self.myArray.append(titleTextField.text!)
+            self.tableViewOutlet.reloadData()
         }))
         
         // 4. Present the alert.
